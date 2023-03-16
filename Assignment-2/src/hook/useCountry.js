3ca71771c11cf39c1b9429dd3cbc26af52301e0a
Mughees0 <abdul.mughees009@gmail.com>
2023-03-16 09:46:35 +0200

    Cleared the async function

--- Assignment-2/src/hook/useCountry.js
+++ Assignment-2/src/hook/useCountry.js
@@ -5,13 +5,10 @@ function useCountry(name) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    async function dataFetch() {
-      await fetch(`https://restcountries.com/v3.1/name/${name}`).then((res) => {
-        if (!res.ok) res.json().then((err) => setError(err.message));
-        else res.json().then((resData) => setData(resData));
-      });
-    }
-    dataFetch();
+    fetch(`https://restcountries.com/v3.1/name/${name}`).then((res) => {
+      if (!res.ok) res.json().then((err) => setError(err.message));
+      else res.json().then((resData) => setData(resData));
+    });
   }, [name]);
   return [error, data];
 }
